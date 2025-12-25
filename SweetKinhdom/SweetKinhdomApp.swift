581ba3e688a831deb77878f62d03d88266c3f0a9
keshew <artyom.korotckov2017@yandex.ru>
2025-12-25 14:23:21 +0500

    Initial Commit

---
+++ SweetKinhdom/SweetKinhdomApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetKinhdomApp.swift
+//  SweetKinhdom
+//
+//  Created by Артём Коротков on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SweetKinhdomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
